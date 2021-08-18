d4882a8093e61b390f42bafeb1c1a6c8daef3056
hainn-98 <ngochai98hp@gmail.com>
2021-08-18 08:53:55 +0700

    Add url patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,4 +5,9 @@ from django.conf.urls import url, include
 urlpatterns = [
     url('api/', include('users.urls')),
     url('api/', include('clients.urls')),
+    url('api/', include('cpis.urls')),
+    url('api/', include('iips.urls')),
+    url('api/', include('import_exports.urls')),
+    url('api/', include('revenue_expenditures.urls')),
+    url('api/', include('unemployments.urls'))
 ]
