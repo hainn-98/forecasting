e302a78e433170de6802019e884ab9376466a6e1
hainn-98 <ngochai98hp@gmail.com>
2021-09-06 15:45:15 +0700

    Edit url

--- backend/iips/urls.py
+++ backend/iips/urls.py
@@ -4,7 +4,7 @@ from .apis import *
 
 urlpatterns = [
     path('iips/add', AddIipApi.as_view(), name='iip_add'),
-    re_path(r'^iips/list/(?:start=(?P<start>(?:19|20)\d{2}(0?[1-9]|1[012])))&(?:end=(?P<end>(?:19|20)\d{2}(0?[1-9]|1[012])))$', IipListApi.as_view(), name='iip_list'),
+    re_path(r'^iips/list/(?:start=(?P<start>(?:19|20)\d{2}([1-9]|1[012])))&(?:end=(?P<end>(?:19|20)\d{2}([1-9]|1[012])))$', IipListApi.as_view(), name='iip_list'),
     path('iips/update/<int:cpi_id>', UpdateIipApi.as_view(), name='iip_update'),
     path('iips/delete/<int:cpi_id>', DeleteIipApi.as_view(), name='iip_delete'),
 ]
--- backend/import_exports/urls.py
+++ backend/import_exports/urls.py
@@ -5,7 +5,7 @@ from .apis import *
 
 urlpatterns = [
     path('import_exports/add', AddImportExportApi.as_view(), name='import_export_add'),
-    re_path(r'^import_exports/list/(?:start=(?P<start>(?:19|20)\d{2}(0?[1-9]|1[012])))&(?:end=(?P<end>(?:19|20)\d{2}(0?[1-9]|1[012])))$', ImportExportListApi.as_view(), name='import_export_list'),
+    re_path(r'^import_exports/list/(?:start=(?P<start>(?:19|20)\d{2}([1-9]|1[012])))&(?:end=(?P<end>(?:19|20)\d{2}([1-9]|1[012])))$', ImportExportListApi.as_view(), name='import_export_list'),
     path('import_exports/update/<int:import_export_id>', UpdateImportExportApi.as_view(), name='import_export_update'),
     path('import_exports/delete/<int:import_export_id>', DeleteImportExportApi.as_view(), name='import_export_delete'),
 ]
