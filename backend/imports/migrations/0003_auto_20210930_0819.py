09e73fdcded0e1eca12bb9ff55dde08dde20e32d
hainn-98 <ngochai98hp@gmail.com>
2021-09-30 15:22:00 +0700

    Initialize import data on startup server

---
+++ backend/imports/migrations/0003_auto_20210930_0819.py
@@ -0,0 +1,36 @@
+import os
+from django.db import migrations
+from django.conf import settings
+import pandas as pd
+from ..models import Import
+from users.models import User
+
+
+def initialize_import_data(apps, schema_editor):
+    data_path = os.path.join(settings.BASE_DIR, 'utils/data/export_data.xlsx')
+    indexes = ('_import', 'domestic_economy', 'foreign_invested_economy', 'machinery_equipment', 'plastic_material', 'cashew', 'rubber', 'cloth', 
+                'iron_steel', 'animal_feed', 'chemical', 'textile_material', 'metal', 'corn', 'chemical_product', 'textile_yarn', 'electronic_product',
+                'cotton', 'pesticide', 'wood_product', 'medicine')
+    data = pd.read_excel(data_path, header=None)
+    df = data.iloc[:, 1:]
+    for time in range(df.shape[1]):
+        data = df.iloc[:, time]
+        year = data[0].year
+        month = data[0].month
+        data_dict = dict(zip(indexes, tuple(data[1:])))
+        user = User.objects.filter(id=1).first()
+        data_dict['year'] = year
+        data_dict['month'] = month
+        data_dict['creator'] = user
+        data_dict['organization'] = user.client
+        Import.objects.create(**data_dict)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('imports', '0002_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(initialize_import_data),
+    ]
